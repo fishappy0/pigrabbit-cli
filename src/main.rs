e985499a2bb10a314e5bbb736b3bdaa085b85799
Fishappy0 <fishappy0@gmail.com>
2022-07-18 12:43:22 +0700

    Removed unnecessary imports and unused reqwest client

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,14 @@
 mod prclient;
 mod types;
 
-use clap;
 use prclient::PRClient;
-use reqwest;
 use serde_json;
 use std::fs;
-use std::io;
 use tokio;
 use types::*;
 
 #[tokio::main]
 async fn main() {
-    let client = reqwest::Client::new();
     let config_file = fs::read_to_string("config.json").expect("[Pigrabbit] File does not exist!");
     let key_struct: Keys = serde_json::from_str(&config_file).unwrap();
     let mut prclient = PRClient::new(key_struct);
