a1bfd15759112044524c68ff00b7dadd6f651df1
Fishappy0 <fishappy0@gmail.com>
2022-07-21 00:58:13 +0700

    Adding the ability to read keys in the config file and make one if there isn't

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,71 @@
-/// A PRClient stands for PigRabbitClient is technically
-/// a reqwest client with authentication keys that was required by PorkBun API
-///
-/// Therefore, to construct the client, the apikey and secretapikey should be provided
-/// to the PRClient in the Keys struct format before performing any interaction to the API.
-///
-/// An usage example of the PRClient:
-/// ```
-/// let keys_file: String = fs::read_to_string("keys.json").expect("File not found!");
-/// let keys: Keys = serde_json::from_str(&);
-/// let mut client = PRClient::new(keys);
-///
-/// client.retrieve_by_domain_with_id("example.com", "1234567").await;
-/// client.del_by_id("example.com","1234567").await;
-/// ```
-mod prclient;
-mod types;
-
-use prclient::PRClient;
-use serde_json;
-use std::fs;
+use directories::BaseDirs;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::path::Path;
 use tokio;
-use types::*;
+
+// create a new folder in the config directory
+fn create_config_folder(config_dir: &Path) {
+    OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&config_dir)
+        .unwrap();
+}
+// Generate a keypair and save it to the config folder.
+fn create_config_file(json_config_dir: &Path) -> pigrabbit::types::Keys {
+    let mut apikey = "".to_owned();
+    let mut secretapikey = "".to_owned();
+    println!("[PigRabbit] The API and secret API Keys not found!");
+    println!("[PigRabbit] Please enter the API key: ");
+    std::io::stdin().read_line(&mut apikey).unwrap();
+    println!("[PigRabbit] Please enter the secret API key: ");
+    std::io::stdin().read_line(&mut secretapikey).unwrap();
+
+    let key_struct = pigrabbit::types::Keys {
+        apikey: apikey.trim().to_string(),
+        secretapikey: secretapikey.trim().to_string(),
+    };
+    serde_json::to_writer(File::create(&json_config_dir).unwrap(), &key_struct).unwrap();
+    key_struct
+}
+// Grab the keypair from the config file or generate a new one if it doesn't exist.
+fn get_keys(config_file: &Path) -> pigrabbit::types::Keys {
+    match std::fs::read_to_string(config_file) {
+        Ok(v) => serde_json::from_str(&v).expect("Failed to parse config file"),
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => create_config_file(config_file),
+        // Panic if there is an error other than file not found.
+        Err(e) => panic!("{}", e),
+    }
+}
+
+fn check_and_create_dir(config_dir: &Path) {
+    std::fs::read_dir(config_dir)
+        .map_err(|e| match e {
+            e if e.kind() == std::io::ErrorKind::NotFound => create_config_folder(config_dir),
+            e => println!("Error: {}", e),
+        })
+        .unwrap();
+}
 
 #[tokio::main]
 async fn main() {
-    let config_file = fs::read_to_string("config.json").expect("[Pigrabbit] File does not exist!");
-    let key_struct: Keys = serde_json::from_str(&config_file).unwrap();
-    let mut prclient = PRClient::new(key_struct);
-    prclient
-        .retreive_by_domain_with_id(
-            // "A".to_owned(),
-            "pornhub.com",
-            "", // None,
+    let base_dirs = BaseDirs::new().unwrap();
+    let config_dir = base_dirs.config_dir().join("pigrabbit-cli");
+    let json_config_dir = config_dir.join("config.json");
+
+    check_and_create_dir(&config_dir);
+    let key_struct = get_keys(&json_config_dir);
+
+    let mut prclient = pigrabbit::PRClient::new(key_struct);
+    let result = prclient
+        .retreive_ssl_by_domain(
+            "arvinderd.com",
+            // "", // none,
         )
-        .await;
+        .await
+        .unwrap();
+
+    // print result
+    println!("{:#?}", result);
 }
